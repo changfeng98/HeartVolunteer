c33aebb648e41cd3fa0f117154d389a2bae629a9
GolThr <1344006993@qq.com>
2019-08-06 12:46:08 +0800

    GolThr(8006)

--- js/helpPage.js
+++ js/helpPage.js
@@ -1,5 +1,49 @@
 function init() {
     initUser();
     $(".help_page").css({background: '#ff664b'});
-
+    $("#help_menu_reg").css({background: "#fff6f6"});
 }
+
+$("#help_menu_reg").click(function (e) {
+    $("#help_menu_reg").css({background: "#fff6f6"});
+    $("#help_menu_acc").css({background: "#fff"});
+    $("#help_menu_web").css({background: "#fff"});
+    $("#help_menu_cus").css({background: "#fff"});
+    $(".account_question").show();
+    $(".activity_organized").hide();
+    $(".website_instructions").hide();
+    $(".contact_us").hide();
+});
+
+$("#help_menu_acc").click(function (e) {
+    $("#help_menu_reg").css({background: "#fff"});
+    $("#help_menu_acc").css({background: "#fff6f6"});
+    $("#help_menu_web").css({background: "#fff"});
+    $("#help_menu_cus").css({background: "#fff"});
+    $(".account_question").hide();
+    $(".activity_organized").show();
+    $(".website_instructions").hide();
+    $(".contact_us").hide();
+});
+
+$("#help_menu_web").click(function (e) {
+    $("#help_menu_reg").css({background: "#fff"});
+    $("#help_menu_acc").css({background: "#fff"});
+    $("#help_menu_web").css({background: "#fff6f6"});
+    $("#help_menu_cus").css({background: "#fff"});
+    $(".account_question").hide();
+    $(".activity_organized").hide();
+    $(".website_instructions").show();
+    $(".contact_us").hide();
+});
+
+$("#help_menu_cus").click(function (e) {
+    $("#help_menu_reg").css({background: "#fff"});
+    $("#help_menu_acc").css({background: "#fff"});
+    $("#help_menu_web").css({background: "#fff"});
+    $("#help_menu_cus").css({background: "#fff6f6"});
+    $(".account_question").hide();
+    $(".activity_organized").hide();
+    $(".website_instructions").hide();
+    $(".contact_us").show();
+});
--- js/newsPage.js
+++ js/newsPage.js
@@ -1,4 +1,5 @@
 function init() {
     initUser();
     $(".news_page").css({background: '#ff664b'});
+    $('.video_btn').css({color:'#ff664b'});
 }
