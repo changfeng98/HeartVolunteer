42727d56d0ffda1685ddff17f7fd51eebf7b0e97
suibian-art <3461216038@qq.com>
2019-08-14 21:01:28 +0800

    qq

--- service/already_activity.php
+++ service/already_activity.php
@@ -4,16 +4,12 @@ include("dbconfig.php");
 //已发活动
 $arr=json_decode($_GET['data'],true);
 $name=$arr['name'];
-$sql="select * from participate where activity_name='$name'";
+$sql="select * from activity where Founder='$name'";
 $obj=mysqli_query($link,$sql);
 $array = array();
+
 while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
 {
-    $pp=$rows["activity"];
-    $sql="select * from activity where act_name='$pp'";
-    $obj=mysqli_query($link,$sql);
-    $qq=mysqli_fetch_array($obj);
-    $array[] = $qq;
+    $array[] = $rows;
 }
-// print_r($array);
 echo json_encode($array);
