59bc2bd4c043c0239932f9b28d9af8e7db3a66b4
suibian-art <3461216038@qq.com>
2019-08-14 20:19:20 +0800

    qq

--- service/partake_post_word.php
+++ service/partake_post_word.php
@@ -4,13 +4,13 @@
   $name=$arr["name"];//发布者
   $word=$arr["word"];//搜索关键词
 
-  $sql="select * from topic_post where user_name='$name'";
+  $sql="select * from topic_post where user_name='$name'and post_name like '%$word%'";
   $obj=mysqli_query($link,$sql);
   $array = array();
   while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
   {
-     // $topic_name=$rows["post_name"];
-      $sql="select * from community where topic_name='%$word%'";
+      $topic_name=$rows["post_name"];
+      $sql="select * from community where topic_name='$topic_name'";
       $obj=mysqli_query($link,$sql);
       $arr=mysqli_fetch_array($obj);
       $array[] = $arr;
