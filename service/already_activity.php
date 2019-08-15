b2d5f101c1b12a18b320530594be532aa606173f
suibian-art <3461216038@qq.com>
2019-08-15 10:52:11 +0800

    qq

--- service/already_activity.php
+++ service/already_activity.php
@@ -4,8 +4,10 @@ include("dbconfig.php");
 //已发活动
 $arr=json_decode($_GET['data'],true);
 $name=$arr['name'];
+
 $sql="select * from activity where Founder='$name'";
 $obj=mysqli_query($link,$sql);
+
 $array = array();
 
 while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
@@ -13,3 +15,4 @@ while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
     $array[] = $rows;
 }
 echo json_encode($array);
+//127.0.0.1/HeartVolunteer/service/already_activity.php?data={"name":"心田地"}
--- service/already_activity_word.php
+++ service/already_activity_word.php
@@ -3,13 +3,13 @@
  $arr=json_decode($_GET['data'],true);
  $name=$arr["name"];//发布活动组织
  $word=$arr["word"];//搜索关键词
-$sql="select * from activity where act_name like '%$word%' and Founder='$name'";
-$obj=mysqli_query($link,$sql);
-//var_dump($obj);
-$array = array();
-while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
-{
-    $array[] = $rows;
-}
+ $sql="select * from activity where act_name like '%$word%' and Founder='$name'";
+ $obj=mysqli_query($link,$sql);
+  //var_dump($obj);
+ $array = array();
+ while($rows=mysqli_fetch_array($obj,MYSQLI_ASSOC))
+ {
+      $array[] = $rows;
+ }
 
-echo json_encode($array);
+  echo json_encode($array);
